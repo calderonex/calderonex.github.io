0624ff64c653d07983c8048eea2b6fc7a4260db0
Maria Vera <maria.vera5@correo.uis.edu.co>
2025-05-13 22:02:18 -0500

    change next config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,21 @@
 import type { NextConfig } from "next";
 import path from "path";
 
+const repoName = 'website'; // nombre del repositorio
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-
-  output: 'export', 
+  output: 'export',
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   webpack(config) {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
-  },};
+  },
+};
 
 export default nextConfig;
-
-
